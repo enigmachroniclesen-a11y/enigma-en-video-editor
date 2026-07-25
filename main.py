b10f791ba6b1dba66caaf53c7755df563bf09a4f
enigmachroniclesen-a11y <enigmachronicles.en@gmail.com>
2026-07-25 15:12:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import PIL.Image
 if not hasattr(PIL.Image, 'ANTIALIAS'):
-PIL.Image.ANTIALIAS = PIL.Image.Resampling.LANCZOS
+PIL.Image.ANTIALIAS = PIL.Image.LANCZOS
 import os
 import re
 import requests
